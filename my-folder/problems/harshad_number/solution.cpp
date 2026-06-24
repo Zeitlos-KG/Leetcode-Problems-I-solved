a19611bb7df34b359e59c16aad5e2d8ef77a5221
Krishiv Gupta <gupta.krishiv2507@gmail.com>
2026-06-24 11:13:33 +0000

    Sync LeetCode submission - Harshad Number (cpp)

---
+++ my-folder/problems/harshad_number/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        int temp = x;
+        int digitSum = 0;
+        while(temp!=0){
+            digitSum+=temp%10;
+            temp/=10;
+        }
+        if(x%digitSum==0) return digitSum;
+        else return -1;
+    }
+};
