df0497740eea026480fad21f13a5e17d99e14a21
Krishiv Gupta <gupta.krishiv2507@gmail.com>
2026-07-18 14:33:08 +0000

    Sync LeetCode submission - Rearrange String to Avoid Character Pair (cpp)

---
+++ my-folder/problems/rearrange_string_to_avoid_character_pair/solution.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string rearrangeString(string s, char x, char y) {
+        if(x>y){sort(s.begin(),s.end());}
+        else {sort(s.begin(),s.end(),greater<int>());}
+        return s;
+    }
+};
